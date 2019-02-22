bd6644981b8672a629c8eba356a8410f600deb2a
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-22 09:41:59 -0500

    got the color picker to work and the background color changes. also got the hsl value to display on the screen and change dynamically

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  state = {
+    hue: '',
+    sat: '',
+    light: '',
+    color: []
+  }
+
+  hueChoice = (event) => {
+    let hueColor = this.state.hue
+    console.log(event)
+    this.setState({
+      hue: event.target.value,
+      // sat: event.target.value,
+      // light: event.target.value,
+      color: this.state.color.concat(hueColor)
+    })
+  }
+
+  satChoice = (event) => {
+    this.setState({
+      sat: event.target.value
+    })
+  }
+
+  lightChoice = (event) => {
+    this.setState({
+      light: event.target.value
+    })
+  }
+
   render() {
-    return <HelloWorld />
+    // return <HelloWorld />
+    return (
+      // <div style={{ backgroundColor: `hsl(50,8%,20%)` }} />
+      <main style={{ backgroundColor: `hsl(${this.state.hue},${this.state.sat}%,${this.state.light}%)` }}>
+        {/* <div style={{ backgroundColor: `hsl(${this.state.hue},${this.state.sat}%,${this.state.light}%)` }} /> */}
+        <h1>Pick a Color, Any Color</h1>
+        <section>
+          <span>
+            <h1>H</h1>
+          </span>
+          <input type="range" min="0" max="240" onChange={this.hueChoice} />
+          <span>
+            <h1>S</h1>
+          </span>
+          <input type="range" onChange={this.satChoice} />
+          <span>
+            <h1>L</h1>
+          </span>
+          <input type="range" onChange={this.lightChoice} />
+        </section>
+        <section>
+          <p>
+            hsl({this.state.hue}, {this.state.sat}%, {this.state.light}%)
+          </p>
+        </section>
+      </main>
+    )
   }
 }
 
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 
 class HelloWorld extends Component {
+  // <div style={{ backgroundColor: `hsl(50,8%,20%)` }} />
   render() {
-    return <h1>Hello, World!</h1>
+    return
+    ;<main>
+      <h1>Pick a Color, Any Color</h1>
+      <main>
+        <input type="range" min="0" max="240" />
+        <input type="range" />
+        <input type="range" />
+      </main>
+    </main>
   }
 }
 
